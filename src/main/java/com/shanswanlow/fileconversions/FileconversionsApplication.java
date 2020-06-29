b5c82b53921a22e138f1b0fda10e7f8c24fa7acf
shans96 <shanswanlow96@gmail.com>
2020-06-29 13:46:35 +0200

    Initial commit

---
+++ src/main/java/com/shanswanlow/fileconversions/FileconversionsApplication.java
@@ -0,0 +1,13 @@
+package com.shanswanlow.fileconversions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileconversionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileconversionsApplication.class, args);
+	}
+
+}
