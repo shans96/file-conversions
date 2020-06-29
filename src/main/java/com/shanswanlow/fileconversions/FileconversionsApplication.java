6ba0622a1aeced3f4d1c96e533b8076ba109c525
shans96 <shanswanlow96@gmail.com>
2020-06-29 15:38:22 +0200

    Change formatting

--- src/main/java/com/shanswanlow/fileconversions/FileconversionsApplication.java
+++ src/main/java/com/shanswanlow/fileconversions/FileconversionsApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FileconversionsApplication {
-
+public class FileconversionsApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(FileconversionsApplication.class, args);
 	}
-
 }
