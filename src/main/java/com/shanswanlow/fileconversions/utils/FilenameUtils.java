9c2650c035d3b9c795d9480763b1f36fa8269cad
shans96 <shanswanlow96@gmail.com>
2020-07-03 16:09:20 +0200

    Prevent instantiation

--- src/main/java/com/shanswanlow/fileconversions/utils/FilenameUtils.java
+++ src/main/java/com/shanswanlow/fileconversions/utils/FilenameUtils.java
@@ -2,6 +2,11 @@ package com.shanswanlow.fileconversions.utils;
 
 public class FilenameUtils
 {
+    private FilenameUtils()
+    {
+        throw new IllegalStateException("Utility class, cannot be instantiated");
+    }
+
     public static String removeExtension(String filename)
     {
         return filename
